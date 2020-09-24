1bc56b8a8c14cda84c245a1fac59d75e4f45152f
tch180 <thopstadredner@gmail.com>
2020-09-23 20:46:09 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // const bugs
 const app = express();
@@ -14,6 +15,16 @@ app.use('/api/bugs', require('./routes/bugs'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/articles', require('./routes/articles'));
+
+//Serve production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 //PORT
 const PORT = process.env.PORT || 8080;
 
